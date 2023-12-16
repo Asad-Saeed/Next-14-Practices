9267b33024be128f67be74df3ee83c696eb9a3e7
Asad Saeed <asadsaeed12225@gmail.com>
2023-12-16 17:41:21 +0500

    feat:Next Auth Credential Login

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -3,7 +3,8 @@ import { z } from 'zod';
 import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
-
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
 /////////////////////////////////////Create Invoice /////////////////////
 
 const FormSchema = z.object({
@@ -123,5 +124,25 @@ export async function deleteInvoice(id: string) {
     return { message: 'Deleted Invoice.' };
   } catch (error) {
     return { message: 'Database Error: Failed to Delete Invoice.' };
+  }
+}
+
+///////////////////////////////////// Authentication /////////////////////
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
   }
 }
