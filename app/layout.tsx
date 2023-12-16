75cd105cb46994eb336339fc0f7754ebd07078d8
Asad Saeed <asadsaeed12225@gmail.com>
2023-12-16 18:09:37 +0500

    feat:Metadata Added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,16 @@
-import "@/app/ui/global.css"
+import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard', //%s  for tiitle tamplate for other pages with company name
+    default: 'Acme Dashboard',
+  },
+  description: 'The official Next.js 14 Dashboard, built with App Router.',
+  metadataBase: new URL('http://localhost:3000/dashboard'),
+};
+
 export default function RootLayout({
   children,
 }: {
@@ -7,7 +18,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={`${inter.className} antialiased`}>
-       <body>{children}</body>
+      <body>{children}</body>
     </html>
   );
 }
